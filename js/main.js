6beee699dbe1be4f0fac5228e685a537330cdbe1
Nick Nance <nicknance@Nicks-MacBook-Pro.local>
2014-11-05 21:12:12 -0500

    Made the collision detection function more efficient

--- js/main.js
+++ js/main.js
@@ -120,18 +120,18 @@ function checkCollision() {
 }
 
 function collision($div1, $div2) {
-	var x1 = $div1.offset().left;
-	var y1 = $div1.offset().top;
-	var h1 = $div1.outerHeight(true);
-	var w1 = $div1.outerWidth(true);
-	var b1 = y1 + h1;
-	var r1 = x1 + w1;
-	var x2 = $div2.offset().left;
-	var y2 = $div2.offset().top;
-	var h2 = $div2.outerHeight(true);
-	var w2 = $div2.outerWidth(true);
-	var b2 = y2 + h2;
-	var r2 = x2 + w2;
+	var off1 = $div1.offset();
+	var off2 = $div2.offset();
+
+	var x1 = off1.left;
+	var y1 = off1.top;
+	var b1 = y1 + 6;
+	var r1 = x1 + 15;
+
+	var x2 = off2.left;
+	var y2 = off2.top;
+	var b2 = y2 + 50;
+	var r2 = x2 + 50;
 
 	if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
 	return true;
